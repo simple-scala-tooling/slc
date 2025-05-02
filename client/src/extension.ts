208500732b7152ecdd739a226a94edb0fe45de7a
rochala <rochala@tutanota.com>
2025-05-03 00:17:00 +0200

    Simplify to single workspace

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,55 +1,16 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-import {
-	workspace as Workspace, window as Window, ExtensionContext, TextDocument, OutputChannel, WorkspaceFolder, Uri
-} from 'vscode';
+import { workspace, ExtensionContext } from 'vscode';
 
 import {
-	LanguageClient, LanguageClientOptions, TransportKind
+	LanguageClient,
+	LanguageClientOptions,
+	TransportKind
 } from 'vscode-languageclient/node';
 
-let defaultClient: LanguageClient;
-const clients = new Map<string, LanguageClient>();
-
-let _sortedWorkspaceFolders: string[] | undefined;
-function sortedWorkspaceFolders(): string[] {
-	if (_sortedWorkspaceFolders === void 0) {
-		_sortedWorkspaceFolders = Workspace.workspaceFolders ? Workspace.workspaceFolders.map(folder => {
-			let result = folder.uri.toString();
-			if (result.charAt(result.length - 1) !== '/') {
-				result = result + '/';
-			}
-			return result;
-		}).sort(
-			(a, b) => {
-				return a.length - b.length;
-			}
-		) : [];
-	}
-	return _sortedWorkspaceFolders;
-}
-Workspace.onDidChangeWorkspaceFolders(() => _sortedWorkspaceFolders = undefined);
-
-function getOuterMostWorkspaceFolder(folder: WorkspaceFolder): WorkspaceFolder {
-	const sorted = sortedWorkspaceFolders();
-	for (const element of sorted) {
-		let uri = folder.uri.toString();
-		if (uri.charAt(uri.length - 1) !== '/') {
-			uri = uri + '/';
-		}
-		if (uri.startsWith(element)) {
-			return Workspace.getWorkspaceFolder(Uri.parse(element))!;
-		}
-	}
-	return folder;
-}
+let client: LanguageClient;
+const supportedFileExtensions = ['scala', 'java', 'mill', 'sbt', 'sc'];
 
 export function activate(context: ExtensionContext) {
-
-	// const module = context.asAbsolutePath(path.join('java', '-jar', '../out/sls/assembly.dest/out.jar'));	
-	const outputChannel: OutputChannel = Window.createOutputChannel('Simple Language Server');
+	// The server is implemented in node
 	const serverJarPath = context.asAbsolutePath("../simple-language-server/out/sls/assembly.dest/out.jar");
 	const serverOptions = {
 		run: {
@@ -68,75 +29,32 @@ export function activate(context: ExtensionContext) {
 		}
 	};
 
-	// Define supported file extensions
-	const supportedFileExtensions = ['scala', 'java', 'mill', 'sbt'];
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: supportedFileExtensions.map(ext => ({
+			scheme: 'file',
+			language: ext,
+		})),
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
 
-	function didOpenTextDocument(document: TextDocument): void {
-		// We are only interested in language mode text
-		console.log(document);
-		console.log(document.languageId);
-		if (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled') {
-			return;
-		}
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
 
-		const uri = document.uri;
-		// Untitled files go to a default client.
-		if (uri.scheme === 'untitled' && !defaultClient) {
-			const clientOptions: LanguageClientOptions = {
-				documentSelector: supportedFileExtensions.map(ext => ({ scheme: 'untitled', language: ext })),
-				diagnosticCollectionName: 'Simple Language Server',
-				outputChannel: outputChannel
-			};
-			defaultClient = new LanguageClient('Scala Language Client', 'Scala Language Server', serverOptions, clientOptions);
-			defaultClient.start();
-			return;
-		}
-		let folder = Workspace.getWorkspaceFolder(uri);
-		// Files outside a folder can't be handled. This might depend on the language.
-		// Single file languages like JSON might handle files outside the workspace folders.
-		if (!folder) {
-			return;
-		}
-		// If we have nested workspace folders we only start a server on the outermost workspace folder.
-		folder = getOuterMostWorkspaceFolder(folder);
-
-		if (!clients.has(folder.uri.toString())) {
-			const clientOptions: LanguageClientOptions = {
-				documentSelector: supportedFileExtensions.map(ext => ({
-					scheme: 'file',
-					language: ext,
-					pattern: `${folder.uri.fsPath}/**/*`
-				})),
-				diagnosticCollectionName: 'Simple Language Server',
-				workspaceFolder: folder,
-				outputChannel: outputChannel
-			};
-			const client = new LanguageClient('Scala Language Client', 'Scala Language Server', serverOptions, clientOptions);
-			client.start();
-			clients.set(folder.uri.toString(), client);
-		}
-	}
-
-	Workspace.onDidOpenTextDocument(didOpenTextDocument);
-	Workspace.textDocuments.forEach(didOpenTextDocument);
-	Workspace.onDidChangeWorkspaceFolders((event) => {
-		for (const folder of event.removed) {
-			const client = clients.get(folder.uri.toString());
-			if (client) {
-				clients.delete(folder.uri.toString());
-				client.stop();
-			}
-		}
-	});
+	// Start the client. This will also launch the server
+	client.start();
 }
 
-export function deactivate(): Thenable<void> {
-	const promises: Thenable<void>[] = [];
-	if (defaultClient) {
-		promises.push(defaultClient.stop());
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
 	}
-	for (const client of clients.values()) {
-		promises.push(client.stop());
-	}
-	return Promise.all(promises).then(() => undefined);
+	return client.stop();
 }
